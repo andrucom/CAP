8da36de0140d46ef9c8f9f157cf8ac4a0faa5726
Бендер <andrucom123@gmail.com>
2024-11-06 17:39:42 +0300

    std:: time

--- CAP.cpp
+++ CAP.cpp
@@ -8,12 +8,12 @@
 #include <cassert>
 #include <vector>
 
-char ver[10] = "0.2.2";
+char ver[10] = "0.2.2 (2)";
 char author[44] = "Чупиков Андрей | ostap_bender";
 int inp;
 
 using hwinfo::unit::bytes_to_MiB;
-using namespace std;
+
 
 const auto cpus = hwinfo::getAllCPUs();
 const auto gpus = hwinfo::getAllGPUs();
@@ -26,66 +26,67 @@ const auto net = hwinfo::getAllNetworks();
 
 int main()
 {
-	cout << "Версия" << "\t\t" << ver << "\n";
-	cout << "Разработчик" << "\t" << author << "\n";
-	cout << "--------------------------------------------" << "\n\n\n";
+	std::cout << "Версия" << "\t\t" << ver << "\n";
+	std::cout << "Разработчик" << "\t" << author << "\n";
+	std::cout << "--------------------------------------------" << "\n\n\n";
 	
 
 
 	do {
-		cout << "\n0 - Завершение программы\n1 - CPU\t\t\t\t2 - GPU\t\t3 - Материнская плата\t\t4 - Диск\n5 - Оперативная память\t\t6 - Батарея\t7 - Оперативная система (OS)\t8 - Cеть \n88 - Благодарность\n ";
-		cout << "\n>>Ввод:"; 
-		cin >> inp;
+		std::cout << "\n\n1 - CPU\t\t\t\t2 - GPU\t\t3 - Материнская плата\t\t4 - Диск\n5 - Оперативная память\t\t6 - Батарея\t7 - Оперативная система (OS)\t8 - Cеть \n88 - Благодарность\n ";
+		std::cout << "\n>>Ввод: "; 
+		std::cin >> inp;
 
 		switch (inp)
 		{
 			case 0:
-				cout << "\n\n << ЗАВЕРШЕНИЕ ПРОГРАММЫ >>" << "\n\n";
+				std::cout << "\n\n << ЗАВЕРШЕНИЕ ПРОГРАММЫ >>" << "\n\n";
 				break;
 			case 1:
 				system("CLS");
 
 				for (const auto& cpu : cpus)
 				{
-					cout << " Название: " << "\t\t\t" << cpu.modelName() << "\n";
-					cout << " Физические ядра: " << "\t\t" << cpu.numPhysicalCores() << "\n";
-					cout << " Логические ядра: " << "\t\t" << cpu.numLogicalCores() << "\n";
-					cout << " Макс. Частота: " << "\t\t" << cpu.maxClockSpeed_MHz() - 1 << " GHz" << "\n";
-					cout << " Частота по умолчанию: " << "\t\t" << cpu.regularClockSpeed_MHz() - 1 << " GHz" << "\n\n\n";
-					cout << " Размер кэша:" << "\n";
-					cout << " >L1 \t\t\t\t" << cpu.L1CacheSize_Bytes() << " Кб\n";
-					cout << " >L2 \t\t\t\t" << cpu.L2CacheSize_Bytes() << " Кб\n";
-					cout << " >L3 \t\t\t\t" << cpu.L3CacheSize_Bytes() << " Кб\n\n";
+					std::cout << " Название: " << "\t\t\t" << cpu.modelName() << "\n";
+					std::cout << " Физические ядра: " << "\t\t" << cpu.numPhysicalCores() << "\n";
+					std::cout << " Логические ядра: " << "\t\t" << cpu.numLogicalCores() << "\n";
+					std::cout << " Макс. Частота: " << "\t\t" << cpu.maxClockSpeed_MHz() << " GHz" << "\n";
+					std::cout << " Частота по умолчанию: " << "\t\t" << cpu.regularClockSpeed_MHz() << " GHz" << "\n\n\n";
+					std::cout << " Макс. Частота: " << "\t\t" << "\n";
+					std::cout << " Размер кэша:" << "\n";
+					std::cout << " >L1 \t\t\t\t" << cpu.L1CacheSize_Bytes() << " Кб\n";
+					std::cout << " >L2 \t\t\t\t" << cpu.L2CacheSize_Bytes() << " Кб\n";
+					std::cout << " >L3 \t\t\t\t" << cpu.L3CacheSize_Bytes() << " Кб\n\n";
 				}
-				cout << "------------------------------------------\n->1 CPU\n";
+				std::cout << "------------------------------------------\n->1 CPU\n";
 				break;
 			case 2:
 				system("CLS");
 
 				for (const auto& gpu : gpus)
 				{
-					cout << " Производитель: " << "\t\t" << gpu.vendor() << "\n";
-					cout << " Наименование: " << "\t\t\t" << gpu.name() << "\n";
-					cout << " Память: " << "\t\t\t" << bytes_to_MiB(gpu.memory_Bytes()) << " MB" << "\n";
-					cout << " Версия драйвера: " << "\t\t" << gpu.driverVersion() << "\n";
-					cout << " vendor_id: " << "\t\t\t" << gpu.vendor_id() << "\n";
-					cout << " device_id: " << "\t\t\t" << gpu.device_id() << "\n\n";
+					std::cout << " Производитель: " << "\t\t" << gpu.vendor() << "\n";
+					std::cout << " Наименование: " << "\t\t\t" << gpu.name() << "\n";
+					std::cout << " Память: " << "\t\t\t" << bytes_to_MiB(gpu.memory_Bytes()) << " MB" << "\n";
+					std::cout << " Версия драйвера: " << "\t\t" << gpu.driverVersion() << "\n";
+					std::cout << " vendor_id: " << "\t\t\t" << gpu.vendor_id() << "\n";
+					std::cout << " device_id: " << "\t\t\t" << gpu.device_id() << "\n\n";
 
 				}
 
-				cout << "------------------------------------------\n->2 GPU\n";
+				std::cout << "------------------------------------------\n->2 GPU\n";
 
 				break;
 			case 3:
 				system("CLS");
 
-				cout << " Производитель: " << "\t\t" << mainboard.vendor() << "\n";
-				cout << " Наименование: " << "\t\t\t" << mainboard.name() << "\n";
-				cout << " Версия: " << "\t\t\t" << mainboard.version() << "\n";
-				cout << " Серийный номер: " << "\t\t" << mainboard.serialNumber() << "\n\n";
+				std::cout << " Производитель: " << "\t\t" << mainboard.vendor() << "\n";
+				std::cout << " Наименование: " << "\t\t\t" << mainboard.name() << "\n";
+				std::cout << " Версия: " << "\t\t\t" << mainboard.version() << "\n";
+				std::cout << " Серийный номер: " << "\t\t" << mainboard.serialNumber() << "\n\n";
 				
 
-				cout << "------------------------------------------\n->3 Материнская плата\n";
+				std::cout << "------------------------------------------\n->3 Материнская плата\n";
 				break;
 
 			case 4: 
@@ -93,14 +94,14 @@ int main()
 
 				for (const auto& disk : disks)
 				{
-					cout << " Производитель: " << "\t\t" << disk.vendor() << "\n";
-					cout << " Наименование: " << "\t\t\t" << disk.model() << "\n";
-					cout << " Память: " << "\t\t\t" << bytes_to_MiB(disk.size_Bytes()) << " MB" << "\n";
-					cout << " Серийный номер: " << "\t\t" << disk.serialNumber() << "\n\n";
+					std::cout << " Производитель: " << "\t\t" << disk.vendor() << "\n";
+					std::cout << " Наименование: " << "\t\t\t" << disk.model() << "\n";
+					std::cout << " Память: " << "\t\t\t" << bytes_to_MiB(disk.size_Bytes()) << " MB" << "\n";
+					std::cout << " Серийный номер: " << "\t\t" << disk.serialNumber() << "\n\n";
 
 				}
 
-				cout << "------------------------------------------\n->4 Диск \n";
+				std::cout << "------------------------------------------\n->4 Диск \n";
 				break; 
 			case 5:
 
@@ -108,19 +109,19 @@ int main()
 				for (const auto& module : ram.modules())
 				{
 					
-					cout << " Производитель: " << "\t\t" << module.vendor << "\n";
-					cout << " Наименование: " << "\t\t\t" << module.name << "\n";
-					cout << " Модель: " << "\t\t\t" << module.model << "\n";
-					cout << " Серийный номер: " << "\t\t" << module.serial_number << "\n";
-					cout << " Память: " << "\t\t\t" << bytes_to_MiB(module.total_Bytes) << " MB" << "\n";
-					cout << " Частота: " << "\t\t\t" << module.frequency_Hz/1000000 << " MHz" << "\n\n";
+					std::cout << " Производитель: " << "\t\t" << module.vendor << "\n";
+					std::cout << " Наименование: " << "\t\t\t" << module.name << "\n";
+					std::cout << " Модель: " << "\t\t\t" << module.model << "\n";
+					std::cout << " Серийный номер: " << "\t\t" << module.serial_number << "\n";
+					std::cout << " Память: " << "\t\t\t" << bytes_to_MiB(module.total_Bytes) << " MB" << "\n";
+					std::cout << " Частота: " << "\t\t\t" << module.frequency_Hz/1000000 << " MHz" << "\n\n";
 				}
 
-				cout << " Память: " << "\t\t" << bytes_to_MiB(ram.total_Bytes()) << "MB" << "\n";
-				cout << " Используется: " << "\t\t" << bytes_to_MiB((ram.total_Bytes() - ram.free_Bytes())) << "MB" << "\n";
-				cout << " Доступно: " << "\t\t" << bytes_to_MiB(ram.free_Bytes()) << "MB" << "\n\n";
+				std::cout << " Память: " << "\t\t" << bytes_to_MiB(ram.total_Bytes()) << "MB" << "\n";
+				std::cout << " Используется: " << "\t\t" << bytes_to_MiB((ram.total_Bytes() - ram.free_Bytes())) << "MB" << "\n";
+				std::cout << " Доступно: " << "\t\t" << bytes_to_MiB(ram.free_Bytes()) << "MB" << "\n\n";
 
-				cout << "------------------------------------------\n->5 Оперативная память\n";
+				std::cout << "------------------------------------------\n->5 Оперативная память\n";
 
 				break;
 			case 6:
@@ -128,23 +129,23 @@ int main()
 				system("CLS");
 				for (const auto& bat : batteries)
 				{
-					cout << " Производитель:" << "\t\t" << bat.getVendor() << "\n";
-					cout << " Модель:" << "\t\t" << bat.getModel() << "\n";
-					cout << " Серийный номер:" << "\t" << bat.getSerialNumber() << "\n";
-					cout << " Технология:" << "\t\t" << bat.getTechnology() << "\n\n";
+					std::cout << " Производитель:" << "\t\t" << bat.getVendor() << "\n";
+					std::cout << " Модель:" << "\t\t" << bat.getModel() << "\n";
+					std::cout << " Серийный номер:" << "\t" << bat.getSerialNumber() << "\n";
+					std::cout << " Технология:" << "\t\t" << bat.getTechnology() << "\n\n";
 				}
-				cout << "------------------------------------------\n->6 Батарея\n";
+				std::cout << "------------------------------------------\n->6 Батарея\n";
 
 				break;
 
 			case 7:
 				system("CLS");
-				cout << " Имя:" << "\t\t" << os.name() << "\n";
-				cout << " Сборка:" << "\t" << os.version() << "\n";
-				cout << " Ядро:" << "\t\t" << os.kernel() << "\n";
-				cout << " 32 бит:" << "\t" << os.is32bit() << "\n";
-				cout << " 64 бит:" << "\t" << os.is64bit() << "\n\n";
-				cout << "------------------------------------------\n->7 Операционная система\n";
+				std::cout << " Имя:" << "\t\t" << os.name() << "\n";
+				std::cout << " Сборка:" << "\t" << os.version() << "\n";
+				std::cout << " Ядро:" << "\t\t" << os.kernel() << "\n";
+				std::cout << " 32 бит:" << "\t" << os.is32bit() << "\n";
+				std::cout << " 64 бит:" << "\t" << os.is64bit() << "\n\n";
+				std::cout << "------------------------------------------\n->7 Операционная система\n";
 
 
 
@@ -155,38 +156,39 @@ int main()
 				system("CLS");
 				for (const auto& net : net)
 				{
-					cout << " interfaceIndex:" << "\t" << net.interfaceIndex() << "\n";
-					cout << " Описание:" << "\t\t" << net.description() << "\n";
-					cout << " MAC:" << "\t\t\t" << net.mac() << "\n";
-					cout << " ip4:" << "\t\t\t" << net.ip4() << "\n";
-					cout << " ip6:" << "\t\t\t" << net.ip6() << "\n\n";
+					std::cout << " interfaceIndex:" << "\t" << net.interfaceIndex() << "\n";
+					std::cout << " Описание:" << "\t\t" << net.description() << "\n";
+					std::cout << " MAC:" << "\t\t\t" << net.mac() << "\n";
+					std::cout << " ip4:" << "\t\t\t" << net.ip4() << "\n";
+					std::cout << " ip6:" << "\t\t\t" << net.ip6() << "\n\n";
 				}
-				cout << "------------------------------------------\n->8 Cеть\n";
+				std::cout << "------------------------------------------\n->8 Cеть\n";
 
 				break;
 
 			default:
 				system("CLS");
 
-				cout << "Версия" << "\t\t" << ver << "\n";
-				cout << "Разработчик" << "\t" << author << "\n";
-				cout << "--------------------------------------------" << "\n\n\n";
+				std::cout << "Версия" << "\t\t" << ver << "\n";
+				std::cout << "Разработчик" << "\t" << author << "\n";
+				std::cout << "--------------------------------------------" << "\n\n\n";
 
 				break;
 
 			case 88:
 				system("CLS");
-				cout << "Хочу выразить свою благодарность своему другу, Dadaskis, спасибо, что решил помочь мне с этой запарой\n\n";
-				cout << "Источники:\n HWinfo - https://github.com/lfreist/hwinfo\n\n";
+				std::cout << "Хочу выразить свою благодарность своему другу, Dadaskis, спасибо, что решил помочь мне с этой запарой\n\n";
+				std::cout << "Источники:\n HWinfo - https://github.com/lfreist/hwinfo\n\n";
 
 				break;
 
 			case 2005:
 				system("CLS");
 				
-				cout << "ДОБРО ПОЖАЛОВАТЬ В INDEX-0, ЭТО ЛУЧШИЙ ГОРОД ИЗ ОСТАВШИХСЯ\n\nhttps://store.steampowered.com/app/2742480/Index_Purger/\n";
-				cout << "------------------------------------------\n->2005 INDEX: PURGER\n";
+				std::cout << "ДОБРО ПОЖАЛОВАТЬ В INDEX-0, ЭТО ЛУЧШИЙ ГОРОД ИЗ ОСТАВШИХСЯ\n\nhttps://store.steampowered.com/app/2742480/Index_Purger/\n";
+				std::cout << "------------------------------------------\n->2005 INDEX: PURGER\n";
 				break;
+
 		}
 	} while (inp != 0);
 
